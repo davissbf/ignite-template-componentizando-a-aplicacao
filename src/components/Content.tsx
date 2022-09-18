3340ced705cf2124cb02e4382c4a91e88556736c
davissbf <davissbf@gmail.com>
2022-09-18 16:49:01 -0300

    Completando desafio de Componentes

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,36 @@
+import { useContext } from "react"
+import { MoviesContext } from "../MoviesContext"
+import { MovieCard } from './MovieCard'
+
+import '../styles/content.scss';
+
+interface MovieProps {
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+        Source: string;
+        Value: string;
+    }>;
+    Runtime: string;
+}
+
+
 export function Content() {
-  // Complete aqui
+    const { movies, selectedGenre} = useContext(MoviesContext);
+
+    return (
+        <div className="container">
+            <header>
+                <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+            </header>
+            <main>
+                <div className="movies-list">
+                    {movies.map((movie: MovieProps) => (
+                        <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+                    ))}
+                </div>
+            </main>
+        </div>
+    )
 }
